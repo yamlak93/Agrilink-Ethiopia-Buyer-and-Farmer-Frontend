d630a25a431951cd3d3454599b73e24aa7f10fba
Yeamlak Firew <95279796+yamlak93@users.noreply.github.com>
2025-12-07 08:49:22 +0300

    Fix case sensitivity in Sidebar.css import

--- src/Buyer/components/Sidebar.jsx
+++ src/Buyer/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next"; // Import translation
-import "../../Css/sidebar.css";
+import "../../Css/Sidebar.css";
 import {
   FaChartBar,
   FaCube,
