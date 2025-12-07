49d49984ae6cb9ff395d85da0fc03590523b22d7
Yeamlak Firew <95279796+yamlak93@users.noreply.github.com>
2025-12-07 08:47:00 +0300

    Rename sidebar.css to Sidebar.css for consistency

--- src/Farmer/components/Sidebar.jsx
+++ src/Farmer/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next"; // ✅ Import translation hook
-import "../../Css/sidebar.css";
+import "../../Css/Sidebar.css";
 import {
   FaChartBar,
   FaCube,
